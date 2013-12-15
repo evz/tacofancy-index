56b69a3c2f597c826387898ad5e48fd5c550ffba
Eric van Zanten <eric@bahai.us>
2013-12-15 10:50:34 -0600

    Initial Commit

---
+++ README.md
@@ -0,0 +1,41 @@
+# TacoFancy Ingredient Index
+
+An attempt at processing recipes stored in the [Tacofancy
+Project](https://github.com/sinker/tacofancy) to build a searchable ingredient
+index. Heavily inspired by the [LA Times Data
+Desk](http://datadesk.latimes.com/posts/2013/12/natural-language-processing-in-the-kitchen/).
+
+### Setting this thing up
+
+Setup a Python virtualenv (using
+[virtualenvwrapper](http://virtualenvwrapper.readthedocs.org/en/latest/)) 
+and ``pip install`` requirements:
+
+``` bash
+    $ mkvirtualenv index
+    $ workon index
+    $ pip install -r requirements.txt
+```
+
+Install NLTK requirements:
+
+``` python
+>>> import nltk
+>>> nltk.download_shell()
+```
+
+This launches an interactive shell session within the python shell that will
+prompt walk you through installing the nltk requirements.
+
+### Make it work
+
+Right now, it’s pretty unimpressive. Invoke the indexer script with a phrase,
+and it’ll try to identify whether or not it’s a recipe step or an ingredient.
+It’s also almost always wrong because there isn’t really much training data and
+I don’t really know what I’m doing. Yet.
+
+``` bash
+    $ python indexer.py "1/2 tbsp salsa"
+    ... training output ...
+    step
+```
---
+++ indexer.py
@@ -0,0 +1,67 @@
+import re
+import requests
+import nltk
+import pickle
+from nltk.classify import MaxentClassifier
+from nltk.tag.simplify import simplify_wsj_tag
+
+training = {
+    'ingredient': [
+        '* 1 packet of Taco Seasoning Mix',
+        '* 1 bag of lentils',
+        '* 1 tbs vegetable oil',
+        '* 1/2 small onion, diced',
+        '* A pile of chiles.',
+        '* 1 cloves garlic, peeled'
+    ],
+    'step': [
+        '1. Set the oven to 350 degrees. Put a Dutch Oven on the range and \
+            apply medium heat.',
+        '2. Chop up your pile of onions. Mince up the garlic. Heck -- get \
+            a beer. This is good beer-drinking-cooking. And the chiles! \
+            Chop them up too. That\'ll get you going.*',
+        '3. Oh yeah -- cut your pork shoulder into hunks of roughly equal \
+            size. I try to keep the hunks as big as possible. Coat it all \
+            over with salt.',
+        '4. Add a tablespoon or so of the oil of your choice to the Dutch \
+            Oven (I use bacon fat). Once it\'s hot, toss in 1/2-1/3 of your \
+            chopped onion. Cook the onion until it turns translucent; add \
+            about half the chopped chiles and garlic, stir well, then add \
+            the pork shoulder. Start them fat-side down, and try to get the \
+            fat in contact with the dutch oven.',
+        '5. Pop the whole works in the oven, uncovered (to start). Grab your \
+            beer, and maybe a book. Time to do some structured waiting.',
+        '6. Every thirty minutes or so, check the pork shoulder. Check the \
+            stuff in the bottom of the pan; as it starts to dry out, throw \
+            in more onions and stir it all around. You can flip the hunks over \
+            after a while -- use tongs.'
+    ]
+}
+
+def train():
+    for k,v in training.items():
+        for sentence in v:
+            words = []
+            sentences = nltk.sent_tokenize(sentence)
+            for sent in sentences:
+                words = words + nltk.word_tokenize(sent)
+            pos = nltk.pos_tag(words)
+            pos = [simplify_wsj_tag(tag) for word, tag in pos]
+            words = [w.lower() for w in words]
+            trigrams = nltk.trigrams(words)
+            trigrams = ['%s/%s/%s' % (i[0], i[1], i[2]) for i in trigrams]
+            features = words + pos + trigrams
+            features = dict((f, True) for f in features)
+            yield (features, k)
+
+if __name__ == '__main__':
+    import sys
+    training_set = []
+    for features in train():
+        training_set.append(features)
+    classifier = MaxentClassifier.train(training_set)
+    print classifier.classify({sys.argv[1]: True})
+
+
+
+
